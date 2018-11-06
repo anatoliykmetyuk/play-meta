817af57b1f290fe1f0148d58c43ccadf5567680e
Anatolii Kmetiuk <anatoliykmetyuk@gmail.com>
2018-11-06 21:01:28 +0200

    out directory removed

--- out/gameforum/model/ForumMessage.scala
+++
@@ -1,9 +0,0 @@
-package gameforum.model
-
-case class ForumMessage(
-  id        : Option[Int] = None
-, content   : String
-, author    : Option[Int] = None
-, topic     : Option[Int] = None
-, created_at: Long)
-    
--- out/gameforum/model/ForumTopic.scala
+++
@@ -1,7 +0,0 @@
-package gameforum.model
-
-case class ForumTopic(
-  id    : Option[Int] = None
-, title : String
-, author: Option[Int] = None)
-    
